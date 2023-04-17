81a65409321c99ca1481c18b1c9a56ef5bc5d4be
Lukebot19 <lukealexander2a@gmail.com>
2023-04-17 18:32:29 +0100

    Command Line interface

--- main.py
+++ main.py
@@ -1,57 +1,70 @@
 import random
-import dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+import dotenv
 import os
+
+
+def main():
+    spotify = spotipy.Spotify(
+        client_credentials_manager=SpotifyOAuth(
+            client_id=os.getenv('SPOTIPY_CLIENT_ID'),
+            client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'),
+            scope=['playlist-modify-private', 'playlist-read-private'],
+            redirect_uri='http://localhost:3000'
+        )
+    )
+
+    playlists = spotify.current_user_playlists()
+
+    # Prompt the user to select a playlist to shuffle
+    print("Choose a playlist to shuffle:")
+    for idx, item in enumerate(playlists['items']):
+        print(f"{idx+1}. {item['name']}")
+    selection = int(input("Enter the number of the playlist: "))
+
+    # Get the ID of the selected playlist
+    playlist_idx = playlists['items'][selection-1]['id']
+
+    song_dict = {}
+
+    playlist = spotify.playlist_items(playlist_id=playlist_idx)
+    num = playlist['total']
+
+    my_count = 0
+
+    while num > 0:
+
+        for idx, song in enumerate(playlist['items']):
+            song_dict[song['track']['name']] = idx + my_count
+
+        num -= 100
+        my_count += 100
+
+        playlist = spotify.playlist_items(
+            playlist_id=playlist_idx, offset=my_count)
+
+    for _ in range(52):
+        song_dict = riffle_shuffle_dict(song_dict)
+
+    for itemidx, item in enumerate(song_dict):
+        spotify.playlist_reorder_items(
+            playlist_id=playlist_idx, range_start=song_dict[item], range_length=1, insert_before=itemidx)
+
+    print("Done")
 
 
 def riffle_shuffle_dict(d):
     items = list(d.items())
     random.shuffle(items)
     half = len(items) // 2
-    shuffled = [items[i//2 + i % 2*half] for i in range(len(items))]
+    shuffled = []
+    for i in range(half):
+        shuffled.append(items[i])
+        shuffled.append(items[i+half])
+    if len(items) % 2 == 1:
+        shuffled.append(items[-1])
     return dict(shuffled)
-
-
-def main():
-    auth_manager = SpotifyOAuth(
-        client_id=os.getenv('SPOTIPY_CLIENT_ID'),
-        client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'),
-        scope=['playlist-modify-private', 'playlist-read-private'],
-        redirect_uri='http://localhost:3000'
-    )
-    spotify = spotipy.Spotify(auth_manager=auth_manager)
-
-    playlists = spotify.current_user_playlists()
-    playlist_name = os.getenv('PLAYLIST_NAME').lower()
-
-    playlist_id = next((item['id'] for item in playlists['items']
-                       if item['name'].lower() == playlist_name), None)
-    if not playlist_id:
-        print(f"Could not find playlist '{playlist_name}'")
-        return
-
-    song_dict = {}
-    playlist = spotify.playlist_items(playlist_id)
-    while True:
-        for idx, song in enumerate(playlist['items']):
-            song_dict[song['track']['name']] = idx + len(song_dict)
-
-        if not playlist['next']:
-            break
-        playlist = spotify.next(playlist)
-
-    for _ in range(52):
-        song_dict = riffle_shuffle_dict(song_dict)
-
-    track_ids = [None] * len(song_dict)
-    for idx, (song_name, pos) in enumerate(song_dict.items()):
-        track_ids[idx] = spotify.search(song_name, type='track')[
-            'tracks']['items'][0]['id']
-
-    spotify.playlist_replace_items(playlist_id, track_ids)
-
-    print("Done")
 
 
 if __name__ == '__main__':
