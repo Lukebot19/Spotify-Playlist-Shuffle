997a80a480eb7a9a40bea038dc389cbe739078b6
Lukebot19 <lukealexander2a@gmail.com>
2023-04-19 17:59:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ from tkinter import messagebox  # import messagebox function from tkinter
 
 
 def main():
+    """
+    Create the main window and widgets for the playlist shuffler.
+    """
+
+    # Spotify authentication and authorization
     spotify = spotipy.Spotify(
         client_credentials_manager=SpotifyOAuth(
             client_id=os.getenv('SPOTIPY_CLIENT_ID'),
@@ -18,6 +23,7 @@ def main():
         )
     )
 
+    # Create the main window
     window = Tk()
     window.title("Playlist Shuffler")
 
@@ -37,11 +43,23 @@ def main():
     for item in playlists['items']:
         playlist_listbox.insert(END, item['name'])
 
+    # Run the main window loop
     window.mainloop()
 
 
 def shuffle_playlist(spotify, playlist_listbox):
+    """
+    Shuffle the selected Spotify playlist.
 
+    Parameters:
+    spotify (spotipy.Spotify): The authenticated Spotify API client.
+    playlist_listbox (tkinter.Listbox): The tkinter Listbox containing the user's playlists.
+
+    Returns:
+    None.
+    """
+
+    # Check if a playlist is selected
     selection = playlist_listbox.curselection()
     if len(selection) == 0:
         messagebox.showerror("Error", "Please select a playlist to shuffle.")
@@ -53,11 +71,13 @@ def shuffle_playlist(spotify, playlist_listbox):
 
     song_dict = {}
 
+    # Get all the songs in the playlist
     playlist = spotify.playlist_items(playlist_id=playlist_idx)
     num = playlist['total']
 
     my_count = 0
 
+    # Build a dictionary of song names and their corresponding index in the playlist
     while num > 0:
 
         for idx, song in enumerate(playlist['items']):
@@ -69,17 +89,29 @@ def shuffle_playlist(spotify, playlist_listbox):
         playlist = spotify.playlist_items(
             playlist_id=playlist_idx, offset=my_count)
 
+    # Shuffle the song dictionary 52 times using a riffle shuffle algorithm
     for _ in range(52):
         song_dict = riffle_shuffle_dict(song_dict)
 
+    # Reorder the playlist based on the shuffled song dictionary
     for itemidx, item in enumerate(song_dict):
         spotify.playlist_reorder_items(
             playlist_id=playlist_idx, range_start=song_dict[item], range_length=1, insert_before=itemidx)
 
+    # Show success message
     messagebox.showinfo("Success", "Playlist shuffled successfully.")
 
 
 def riffle_shuffle_dict(d):
+    """
+    Shuffle a dictionary using a riffle shuffle algorithm.
+
+    Parameters:
+    d (dict): The dictionary to be shuffled.
+
+    Returns:
+    dict: The shuffled dictionary.
+    """
     items = list(d.items())
     random.shuffle(items)
     half = len(items) // 2
